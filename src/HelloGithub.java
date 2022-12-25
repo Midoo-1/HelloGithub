2f7dfa666d3cf1373b9d89649f54dadabc637eec
Mido-1 <mostafakanjo10@gmail.com>
2022-12-25 16:20:43 +0100

    Tarhib fucntion

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -1,4 +1,11 @@
 
 public class HelloGithub {
+	public static void tarhib(String name) {
+		System.out.println("Good morning " + name);
+	}
+	
+	public static void main(String[] args) {
+		tarhib("Moustafa");
+	}
 
 }
